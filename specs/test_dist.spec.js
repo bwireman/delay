a13a0ad94882650059794c186932b88b1771779b
bwireman <bwireman1@gmail.com>
2024-02-24 17:46:48 -0600

    more dist tests

--- specs/test_dist.spec.js
+++ specs/test_dist.spec.js
@@ -1,29 +1,31 @@
-import { run, delay_effect, map, repeat } from "../dist/delay"
-import { Ok, Error } from "./prelude.mjs"
+// smoke tests to establish that the NPM code can be used
+// & works as expected
+import { delay_effect, map, run, repeat, fallthrough } from "../dist/delay"
+import { Ok, Error, List } from "./prelude.mjs"
 
-test('delay_effect', () => {
-    let fin = 0;
+test("delay_effect", () => {
+    let fin = 0
     const d = delay_effect(() => {
-        fin += 1;
+        fin += 1
         return new Ok(fin)
     })
 
-    expect(fin).toBe(0);
+    expect(fin).toBe(0)
 
     let res = run(d)[0]
-    expect(res).toBe(fin);
-    expect(fin).toBe(1);
+    expect(res).toBe(fin)
+    expect(fin).toBe(1)
 
     res = run(d)[0]
-    expect(res).toBe(fin);
-    expect(fin).toBe(2);
+    expect(res).toBe(fin)
+    expect(fin).toBe(2)
 
     res = run(d)[0]
-    expect(res).toBe(fin);
-    expect(fin).toBe(3);
-});
+    expect(res).toBe(fin)
+    expect(fin).toBe(3)
+})
 
-test('delay_effect map', () => {
+test("map", () => {
     let d1 = delay_effect(() => new Ok("HELLO"))
     d1 = map(d1, (v) => new Ok(v + "WORLD"))
     d1 = map(d1, (v) => new Ok(v + "!"))
@@ -33,26 +35,49 @@ test('delay_effect map', () => {
 
     let d2 = delay_effect(() => new Ok("HELLO"))
     d2 = map(d2, (v) => new Ok(v + "WORLD"))
-    d2 = map(d2, (v) => new Error("shit!"))
+    d2 = map(d2, (_) => new Error("shit!"))
 
     const res2 = run(d2)[0]
     expect(res2).toBe("shit!")
-});
+})
 
-test('delay_effect repeat', () => {
-    let fin = 0;
+test("repeat", () => {
+    let fin = 0
     const d = delay_effect(() => {
-        fin += 1;
+        fin += 1
         return new Ok(fin)
     })
 
-    expect(fin).toBe(0);
+    expect(fin).toBe(0)
 
     const res = repeat(d, 3)
         .toArray()
-        .map((x) => x[0]);
+        .map((x) => x[0])
 
     expect(res).toStrictEqual([3, 2, 1])
-    expect(fin).toBe(3);
-});
+    expect(fin).toBe(3)
+})
+
+test("fallthrough", () => {
+    let fin = 0
+    const d = delay_effect(() => {
+        if (fin > 1) {
+            return new Ok("ok!")
+        }
+
+        fin += 1
+        return new Error("err!")
+    })
+
+    expect(fallthrough(List.fromArray([d])).isOk()).toBe(false)
+    fin = 0
+
+    expect(fallthrough(List.fromArray([d, d])).isOk()).toBe(false)
+    fin = 0
+
+    expect(fallthrough(List.fromArray([d, d, d])).isOk()).toBe(true)
+    expect(fallthrough(List.fromArray([d, d, d, d])).isOk()).toBe(true)
+
+    expect(fin).toBe(2)
+})
 
